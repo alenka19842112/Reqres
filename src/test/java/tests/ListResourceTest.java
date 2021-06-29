ec8287ca66493646509cd565e91638439afe4383
aivanov <aivanov@aras.com>
2021-06-29 08:20:13 +0300

    add tests (changes)

--- src/test/java/tests/ListResourceTest.java
+++ src/test/java/tests/ListResourceTest.java
@@ -10,7 +10,7 @@ import static io.restassured.RestAssured.given;
 public class ListResourceTest implements IConstansTest {
 
     @Test
-    public void getDataListResourceTest() {
+    public void getDataAndSupportTextListResourceTest() {
         Resource resource =
                 given().contentType(ContentType.JSON)
                         .log().all()
@@ -22,19 +22,6 @@ public class ListResourceTest implements IConstansTest {
                         .extract().response().as(Resource.class);
         String response = "DataResource(id=2, name=fuchsia rose, year=2001, color=#C74375, pantoneValue=17-2031)";
         Assert.assertEquals(resource.getData().get(1).toString(), response);
-    }
-
-    @Test
-    public void getSupportListResourceTest() {
-        Resource resource =
-                given().contentType(ContentType.JSON)
-                        .log().all()
-                .when()
-                        .get(BASE_URL + "unknown")
-                .then()
-                        .log().all()
-                        .statusCode(200)
-                        .extract().response().as(Resource.class);
         Assert.assertNotNull(resource.getSupport().getText());
     }
 }
--- src/test/java/tests/ListUsersTest.java
+++ src/test/java/tests/ListUsersTest.java
@@ -10,20 +10,6 @@ import static io.restassured.RestAssured.given;
 public class ListUsersTest implements IConstansTest {
 
     @Test
-    public void getListUserTest() {
-        ListUsers listUsers =
-                given().contentType(ContentType.JSON)
-                        .log().all()
-                .when()
-                        .get(BASE_URL + "users?page=2")
-                .then()
-                        .log().all()
-                        .statusCode(200)
-                        .extract().response().as(ListUsers.class);
-        Assert.assertTrue(listUsers.getData().size() > 0);
-    }
-
-    @Test
     public void getDataIdListUserTest() {
         ListUsers listUsers =
                 given().contentType(ContentType.JSON)
@@ -34,6 +20,7 @@ public class ListUsersTest implements IConstansTest {
                         .log().all()
                         .statusCode(200)
                         .extract().response().as(ListUsers.class);
+        Assert.assertTrue(listUsers.getData().size() > 0);
         Assert.assertEquals(listUsers.getData().get(0).getId(), 7);
     }
 }
